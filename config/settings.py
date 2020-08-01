dd9063408bf4b314b11b41263de5344766f2d682
zzangmo <how9309@naver.com>
2020-08-01 20:33:07 +0900

    False

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "&9nk^8d(m5tgw!o!2nodj4uu*+%n=(788i&hsq6i07b44&^@1w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
